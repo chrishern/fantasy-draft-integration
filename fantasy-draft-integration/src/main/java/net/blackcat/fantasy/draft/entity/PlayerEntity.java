987405585206c16f94491b51a21b3d6ee6d500ff
chrishern <chrishern@hotmail.com>
2014-07-21 20:52:41 +0100

    Moved repository contents.

---
+++ fantasy-draft-integration/src/main/java/net/blackcat/fantasy/draft/entity/PlayerEntity.java
@@ -0,0 +1,130 @@
+/**
+ * 
+ */
+package net.blackcat.fantasy.draft.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+//import net.blackcat.fantasy.draft.player.types.Position;
+
+/**
+ * Entity class representing a fantasy draft player.
+ * 
+ * @author Chris
+ *
+ */
+@Entity(name = "Player")
+public class PlayerEntity implements Serializable {	
+
+	private static final long serialVersionUID = 6584040856373261900L;
+
+	@Id
+	private long id;
+	
+	@Column
+	private String forename;
+	
+	@Column
+	private String surname;
+	
+	@Column
+	private String team;
+
+//	@Column
+//	@Enumerated(EnumType.STRING)
+//	private Position position;
+	
+	@Column
+	private int totalPoints;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the forename
+	 */
+	public String getForename() {
+		return forename;
+	}
+
+	/**
+	 * @param forename the forename to set
+	 */
+	public void setForename(String forename) {
+		this.forename = forename;
+	}
+
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
+
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	/**
+	 * @return the team
+	 */
+	public String getTeam() {
+		return team;
+	}
+
+	/**
+	 * @param team the team to set
+	 */
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+//	/**
+//	 * @return the position
+//	 */
+//	public Position getPosition() {
+//		return position;
+//	}
+//
+//	/**
+//	 * @param position the position to set
+//	 */
+//	public void setPosition(Position position) {
+//		this.position = position;
+//	}
+
+	/**
+	 * @return the totalPoints
+	 */
+	public int getTotalPoints() {
+		return totalPoints;
+	}
+
+	/**
+	 * @param totalPoints the totalPoints to set
+	 */
+	public void setTotalPoints(int totalPoints) {
+		this.totalPoints = totalPoints;
+	}
+	
+}
