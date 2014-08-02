2321fa985a192f035e9aa7566c1226823791655b
chrishern <chrishern@hotmail.com>
2014-08-02 15:08:52 +0100

    Corrected JavaDoc spelling mistake.

--- fantasy-draft-integration/src/main/java/net/blackcat/fantasy/draft/integration/facade/DraftRoundFacade.java
+++ fantasy-draft-integration/src/main/java/net/blackcat/fantasy/draft/integration/facade/DraftRoundFacade.java
@@ -22,7 +22,7 @@ public interface DraftRoundFacade {
 	 * 		<ul>
 	 * 			<li><b>LEAGUE_DOES_NOT_EXIST</b> - If the given league does not exist.</li>
 	 * 			<li><b>OPEN_DRAFT_ROUND_ALREADY_EXISTS_FOR_LEAGUE</b> - If the given league already has an open draft round.</li>
-	 * 			<li><b>DRAFT_ROUND_ALREADY_EXISTS_FOR_LEAGUE</b> - If an action round for the given phase number alreadty exists.</li>
+	 * 			<li><b>DRAFT_ROUND_ALREADY_EXISTS_FOR_LEAGUE</b> - If an action round for the given phase number already exists.</li>
 	 * 		</ul>
 	 */
 	void startAuctionPhase(int leagueId, final int phase) throws FantasyDraftIntegrationException;
