7d880f903a2add9ce148fe216506f59f63f9b1f0
chrishern <chrishern@hotmail.com>
2015-07-11 20:00:52 +0100

    Updated useful queries.

--- fantasy-draft-integration/documentation/UsefulQueries.sql
+++ fantasy-draft-integration/documentation/UsefulQueries.sql
@@ -1,7 +1,7 @@
 -- Get all bids made in a given transfer window
 SELECT Bid.team_id, Player.forename, Player.surname, Bid.amount
 FROM TransferWindow_Bid, Bid, Player
-WHERE TransferWindow_Bid.auctionRoundBids_id = Bid.id AND TransferWindow_sequenceNumber = 7 AND Bid.player_id = Player.id
+WHERE TransferWindow_Bid.auctionRoundBids_id = Bid.id AND TransferWindow_sequenceNumber = 10 AND Bid.player_id = Player.id
 ORDER BY Bid.team_id;
 
 
@@ -19,15 +19,15 @@ GROUP BY Team_SelectedPlayer.Team_id;
 -- Get all details of STILL_SELECTED players for a team
 SELECT SelectedPlayer.id, Player.forename, Player.surname, SelectedPlayer.selectionStatus
 FROM SelectedPlayer, Team_SelectedPlayer, Player
-WHERE Team_SelectedPlayer.Team_id = 2
+WHERE Team_SelectedPlayer.Team_id = 5
 		AND Team_SelectedPlayer.selectedPlayers_id = SelectedPlayer.id 
 		AND SelectedPlayer.stillSelected = 'STILL_SELECTED'
 		AND Player.id = SelectedPlayer.player_id;
 
 -- Get details of all players a team has ever had
-SELECT SelectedPlayer.id, Player.forename, Player.surname, SelectedPlayer.stillSelected, SelectedPlayer.currentSellToPotPrice, SelectedPlayer.fplCostAtPurchase
+SELECT SelectedPlayer.id, Player.forename, Player.surname, SelectedPlayer.stillSelected, SelectedPlayer.currentSellToPotPrice, SelectedPlayer.selectionStatus, SelectedPlayer.fplCostAtPurchase
 FROM SelectedPlayer, Team_SelectedPlayer, Player
-WHERE Team_SelectedPlayer.Team_id = 1 
+WHERE Team_SelectedPlayer.Team_id = 6 
 		AND Team_SelectedPlayer.selectedPlayers_id = SelectedPlayer.id 
 		AND Player.id = SelectedPlayer.player_id;
 
@@ -35,25 +35,26 @@ UPDATE SelectedPlayer SET cost = 3.8 WHERE id = 201;
 UPDATE SelectedPlayer SET cost = 6 WHERE id = 196;
 UPDATE SelectedPlayer SET cost = 5.7 WHERE id = 197;
 UPDATE SelectedPlayer SET cost = 2 WHERE id = 200;
+UPDATE SelectedPlayer SET selectionStatus = NULL WHERE id = 219;
 		
 DELETE FROM `Team_SelectedPlayer` WHERE `selectedPlayers_id` = 176;
 
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 70;
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 71;
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 123;
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 129;
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 131;
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 142;
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 148;
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 153;
-UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 186;
-UPDATE SelectedPlayer SET selectionStatus = 'CAPTAIN' WHERE id = 68;
-UPDATE SelectedPlayer SET selectionStatus = 'VICE_CAPTAIN' WHERE id = 69;
-UPDATE SelectedPlayer SET selectionStatus = 'SUB_1' WHERE id = 172;
-UPDATE SelectedPlayer SET selectionStatus = 'SUB_2' WHERE id = 199;
-UPDATE SelectedPlayer SET selectionStatus = 'SUB_3' WHERE id = 206;
-UPDATE SelectedPlayer SET selectionStatus = 'SUB_4' WHERE id = 184;
-UPDATE SelectedPlayer SET selectionStatus = 'SUB_5' WHERE id = 145;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 15;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 20;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 161;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 205;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 222;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 224;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 235;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 236;
+UPDATE SelectedPlayer SET selectionStatus = 'PICKED' WHERE id = 237;
+UPDATE SelectedPlayer SET selectionStatus = 'CAPTAIN' WHERE id = 13;
+UPDATE SelectedPlayer SET selectionStatus = 'VICE_CAPTAIN' WHERE id = 162;
+UPDATE SelectedPlayer SET selectionStatus = 'SUB_1' WHERE id = 22;
+UPDATE SelectedPlayer SET selectionStatus = 'SUB_2' WHERE id = 216;
+UPDATE SelectedPlayer SET selectionStatus = 'SUB_3' WHERE id = 17;
+UPDATE SelectedPlayer SET selectionStatus = 'SUB_4' WHERE id = 204;
+UPDATE SelectedPlayer SET selectionStatus = 'SUB_5' WHERE id = 14;
 
 
 -- Get the names of the players involved in transfers in the current transfer window
@@ -66,10 +67,19 @@ WHERE TransferWindow_Transfer.TransferWindow_sequenceNumber = TransferWindow.seq
 	AND TransferredPlayer.player_id = Player.id;
 	
 	
--- Remove an erroneously submitted transfer
-DELETE FROM TransferWindow_Transfer WHERE transfers_id = 66;
-DELETE FROM Transfer_ExchangedPlayer WHERE Transfer_id = 66;
---DELETE FROM ExchangedPlayer WHERE id = 1;
-DELETE FROM Transfer_TransferredPlayer WHERE Transfer_id = 66;
-DELETE FROM TransferredPlayer WHERE id = 66;
-DELETE FROM Transfer WHERE id = 66;
+-- Remove an erroneously submitted transfer: transfers_id = 90
+DELETE FROM TransferWindow_Transfer WHERE transfers_id = 90;
+DELETE FROM Transfer_ExchangedPlayer WHERE Transfer_id = 90;
+DELETE FROM ExchangedPlayer WHERE id = 6;
+DELETE FROM ExchangedPlayer WHERE id = 7;
+DELETE FROM Transfer_TransferredPlayer WHERE Transfer_id 90;
+DELETE FROM TransferredPlayer WHERE id = 90;
+DELETE FROM Transfer WHERE id = 90;
+
+
+
+-- Get deals in current transfer window
+SELECT *
+FROM Transfer, TransferWindow_Transfer
+WHERE TransferWindow_Transfer.transferWindow_sequenceNumber = 9
+	AND TransferWindow_Transfer.transfers_id = Transfer.id;
