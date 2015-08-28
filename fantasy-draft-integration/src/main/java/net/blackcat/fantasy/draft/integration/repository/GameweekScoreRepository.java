8531d05758de9bf657c65ead06a6969e36a73240
chrishern <chrishern@hotmail.com>
2015-08-28 20:53:37 +0100

    Removed unused query method.

--- fantasy-draft-integration/src/main/java/net/blackcat/fantasy/draft/integration/repository/GameweekScoreRepository.java
+++ fantasy-draft-integration/src/main/java/net/blackcat/fantasy/draft/integration/repository/GameweekScoreRepository.java
@@ -14,9 +14,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface GameweekScoreRepository extends CrudRepository<GameweekScore, Integer>, JpaSpecificationExecutor<GameweekScore> {
 
-	@Query("SELECT gameweekScore FROM Team team JOIN team.gameweekScores gameweekScore WHERE team.id=:teamId AND gameweekScore.id=:gameweek")
-	public GameweekScore getGameweekScoreForTeam1(@Param("teamId") int teamId, @Param("gameweek") int gameweek);
-	
-	@Query(nativeQuery = true, value = "SELECT gs.score FROM GameweekScore gs, Team t, Team_GameweekScore tgs WHERE gs.gameweek=:gameweek AND t.id=:teamId AND tgs.Team_id = t.id AND tgs.gameweekScores_id = gs.id")
+	@Query(nativeQuery = true, value = "SELECT gs.score FROM GameweekScore gs, Team t, Team_GameweekScore tgs "
+			+ "WHERE gs.gameweek=:gameweek AND t.id=:teamId AND tgs.Team_id = t.id AND tgs.gameweekScores_id = gs.id")
 	public List<Object> getGameweekScoreForTeam(@Param("teamId") int teamId, @Param("gameweek") int gameweek);
 }
